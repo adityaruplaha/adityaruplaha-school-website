ff28e8cb1eb620d1572b8e9a57264b0c6d6421f7
adityaruplaha <30696515+adityaruplaha@users.noreply.github.com>
2020-06-14 18:14:38 +0530

    Overhaul contact page.

--- contact/index.php
+++ contact/index.php
@@ -18,42 +18,80 @@ if (!$is_logged_in) {
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.7">
     <link rel='stylesheet' type='text/css' href='stylesheet.css' />
 </head>
 
 <body>
     <h1 align=center>Contact Teachers</h1>
-    <hr />
+    <hr /><br />
     <div>
         <table>
             <tr>
-                <td align=center>Debarati Pramanik</td>
-                <td align=center>Nandita Dastidar</td>
-                <td align=center>Saswati Sur</td>
+                <th>Name</th>
+                <th colspan="3">Phone No.</th>
+                <th>EMail</th>
             </tr>
-            <tr>
-                <td><a href='vcf/debartip.vcf'><img src='img/debaratip.png' /></a></td>
-                <td><a href='vcf/nanditad.vcf'><img src='img/nanditad.png' /></a></td>
-                <td><a href='vcf/saswatis.vcf'><img src='img/saswatis.png' /></a></td>
-            </tr>
-            <tr>
-                <td colspan=3>
-                    <hr />
-                </td>
-            </tr>
-            <tr>
-                <td align=center>Soumi Karmakar</td>
-            </tr>
-            <tr>
-                <td><a href='vcf/soumik.vcf'><img src='img/soumik.png' /></a></td>
-            </tr>
+            <?php
+
+            require_once "../defs.php";
+
+            use const ScA\DB;
+            use const ScA\DB_HOST;
+            use const ScA\DB_PWD;
+            use const ScA\DB_USER;
+
+            $conn = new mysqli(DB_HOST, DB_USER, DB_PWD, DB);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Query
+            $result = $conn->query("SELECT * FROM teachers WHERE Display = 1");
+            if (!$result) {
+                die("Query to show fields from table failed");
+            }
+
+            function get_phone_no_formatted($ph)
+            {
+                $ph_i = intval($ph);
+                $wa = "<a class='img' href='https://wa.me/{$ph_i}'><img class='icon' src='img/wp.png'/></a>";
+                $tel = "<a class='img' href='tel:{$ph}'><img class='icon' src='img/call.png'/></a>";
+                return "<td>{$ph}</td><td>{$wa}</td><td>{$tel}</td>";
+            }
+
+            while ($r = $result->fetch_assoc()) {
+                $rp = 1;
+                if ($r["Mobile2"]) {
+                    $rp = 2;
+                }
+                echo "<tr>";
+
+                echo "<td rowspan={$rp}>";
+                echo $r["Name"];
+                echo "</td>";
+
+                echo get_phone_no_formatted($r["Mobile1"]);
+
+                echo "<td rowspan={$rp}>";
+                echo "<a href='mailto:" . $r["EMail1"] . "'>" . $r["EMail1"] . "</a>";
+                if ($r["EMail2"]) {
+                    echo "<br/>" . "<a href='mailto:" . $r["EMail2"] . "'>" . $r["EMail2"] . "</a>";
+                }
+                echo "</td>";
+
+                echo "</tr>";
+
+                if ($r["Mobile2"]) {
+                    echo "<tr>" . get_phone_no_formatted($r["Mobile2"]) . "</tr>";
+                }
+            }
+
+            ?>
         </table>
     </div>
-    <hr width="67%" />
-    <p align=center id='foot'>
-        Scan the QR codes above to add teachers to your contacts.<br />
-        Alternatively, click on them to download as vCard file.
-    </p>
 </body>
 
 </html>
